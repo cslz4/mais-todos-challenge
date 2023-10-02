e5dab756f98b3a64a39919a3987ed064194a5845
Carlos Sluzala <wdcarloss@gmail.com>
2023-10-02 12:12:11 -0300

    fix: remove unused import

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 
 import './index.css';
 import { HomePage } from './pages/HomePage';
