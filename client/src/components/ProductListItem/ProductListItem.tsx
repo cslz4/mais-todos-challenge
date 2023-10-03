0c124c1f110d877bc91666dfb02aca1bb0b58ac0
Carlos Sluzala <wdcarloss@gmail.com>
2023-10-03 09:28:24 -0300

    fix: product thumb size

--- client/src/components/ProductListItem/ProductListItem.tsx
+++ client/src/components/ProductListItem/ProductListItem.tsx
@@ -14,7 +14,7 @@ export function ProductListItem({ product }: ProductListItemProps) {
     <div className="flex flex-col justify-center">
       <Link to={`/product/${product.slug}`} className="w-full flex justify-center align-center text-gray-800 text-center group hover:cursor-pointer">
         <div className="w-full">
-          <div className="h-full w-full lg:h-96 bg-slate-100 p-14 rounded-lg">
+          <div className="w-full h-64 md:h-72 lg:h-96 bg-slate-100 p-14 rounded-lg">
             <img className="w-full h-full object-scale-down group-hover:scale-[1.05] transition-transform" alt={product.name} src={product.imageUrl} />
           </div>
           <div className="text-gray-400 text-md uppercase font-bold pt-8 tracking-widest">{product.name}</div>
